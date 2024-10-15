633a83935195261bde48f0fad41f644dd10ac751
Adi <nairadityasit@gmail.com>
2024-10-15 16:00:17 +0530

    first commit

---
+++ src/test/java/com/microservice/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,58 @@
+package com.microservice.product_service;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microservice.product_service.model.Product;
+
+@SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
+class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:8.0.1");
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
+	
+	@Test
+	void shouldCreateProduct() throws Exception {
+		
+		Product product = getProductRequest();
+		
+		String productString = objectMapper.writeValueAsString(product);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/product/saveProduct")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productString))
+		.andExpect(status().isCreated());
+	}
+
+	private Product getProductRequest() {    
+		return new Product("HP Probook", "Laptop with a comfortable processing speed with Ryzen 5 graphic card and 16GB RAM", BigDecimal.valueOf(1000));
+	}
+}
